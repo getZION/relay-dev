679337899b8877348a07672fd5139abf56f07eea
Christopher David <chris@arcade.city>
2022-02-25 20:55:17 -0600

    console log in CollectionsWrite

--- api/collections.go
+++ api/collections.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	. "github.com/getzion/relay/gen/proto/identityhub/v1"
+	. "github.com/getzion/relay/utils"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
@@ -14,6 +15,8 @@ type CollectionsService struct {
 func (s *CollectionsService) CollectionsWrite(ctx context.Context, q *CollectionsWriteRequest) (*CollectionsWriteResponse, error) {
 	grpc.SendHeader(ctx, metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-unary"))
 	grpc.SetTrailer(ctx, metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-unary"))
+
+	Log.Info().Msg("CollectionsWrite placeholder")
 
 	return &CollectionsWriteResponse{}, nil
 }
