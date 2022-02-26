6abc910480d1be2af91fc1489b80497690136c65
Christopher David <chris@arcade.city>
2022-02-25 21:22:51 -0600

    axe community create test

--- ui/src/components/dom/RPCTest.tsx
+++ ui/src/components/dom/RPCTest.tsx
@@ -7,11 +7,11 @@ import { Community, CreateCommunityRequest } from 'proto/zion/v1/communities_pb'
 import { NodeInfoService } from 'proto/zion/v1/nodeinfo_pb_service'
 
 const nodeInfoRequest = new GetNodeInfoRequest()
-const createCommunityRequest = new CreateCommunityRequest()
-const community = new Community()
-community.setName('Hello World Community! !! ! !!')
-community.setDescription('AMAZING DESCRIPTION ! ! !! ')
-createCommunityRequest.setPayload(community)
+// const createCommunityRequest = new CreateCommunityRequest()
+// const community = new Community()
+// community.setName('Hello World Community! !! ! !!')
+// community.setDescription('AMAZING DESCRIPTION ! ! !! ')
+// createCommunityRequest.setPayload(community)
 
 interface NodeInfoObject {
   pubkey: string
@@ -40,13 +40,13 @@ export const RPCTest = () => {
       },
     })
 
-    grpc.unary(CommunitiesService.CreateCommunity, {
-      request: createCommunityRequest,
-      host: 'http://localhost:9090',
-      onEnd: (res) => {
-        console.log('CreateCommunity response:', res)
-      },
-    })
+    // grpc.unary(CommunitiesService.CreateCommunity, {
+    //   request: createCommunityRequest,
+    //   host: 'http://localhost:9090',
+    //   onEnd: (res) => {
+    //     console.log('CreateCommunity response:', res)
+    //   },
+    // })
   }, [])
   return <></>
 }
