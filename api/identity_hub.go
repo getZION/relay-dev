a3d62dc97749a8e6b482845d6d089e2d7a460154
chrishein3 <chrishein931@gmail.com>
2022-03-03 16:55:19 +0300

    IdentityHub implemantation refactoring & IdentityHub tests written

--- api/identity_hub.go
+++ api/identity_hub.go
@@ -1,12 +1,15 @@
 package api
 
 import (
-	"errors"
 	"strconv"
 	"time"
 
 	. "github.com/getzion/relay/gen/proto/identityhub/v1"
 	. "github.com/getzion/relay/utils"
+	"github.com/google/uuid"
+
+	//"github.com/ipfs/go-cid"
+	//"github.com/multiformats/go-multihash"
 
 	"github.com/go-playground/validator/v10"
 	"golang.org/x/net/context"
@@ -14,73 +17,124 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-func InitIdentityHubService() *IdentityHubService {
-
-	validator := validator.New()
-	identityHubService := &IdentityHubService{
-		validator: validator,
-	}
-
-	return identityHubService
-}
-
 type IdentityHubService struct {
 	UnimplementedHubRequestServiceServer
 
-	validator *validator.Validate
+	validator                *validator.Validate
+	validHubInterfaceMethods map[string]string
+}
+
+const (
+	requestLevelProcessingErrorMessage string = "The request could not be processed correctly"
+	targetDIDNotFoundErrorMessage      string = "Target DID not found within the Identity Hub instance"
+
+	improperlyConstructedErrorMessage   string = "The message was malformed or improperly constructed"
+	interfaceNotImplementedErrorMessage string = "The interface method is not implemented"
+	authorizationFailedErrorMessage     string = "The message failed authorization requirements"
+)
+
+func InitIdentityHubService() *IdentityHubService {
+
+	validator := validator.New()
+
+	identityHubService := &IdentityHubService{
+		validator: validator,
+		validHubInterfaceMethods: map[string]string{
+			"CollectionsQuery":   "",
+			"CollectionsWrite":   "",
+			"CollectionsCommit":  "",
+			"CollectionsDelete":  "",
+			"ThreadsQuery":       "",
+			"ThreadsCreate":      "",
+			"ThreadsReply":       "",
+			"ThreadsClose":       "",
+			"ThreadsDelete":      "",
+			"PermissionsRequest": "",
+			"PermissionsQuery":   "",
+			"PermissionsGrant":   "",
+			"PermissionsRevoke":  "",
+		},
+	}
+
+	return identityHubService
 }
 
 func (hub *IdentityHubService) Process(ctx context.Context, r *Request) (*Response, error) {
 	grpc.SendHeader(ctx, metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-unary"))
 	grpc.SetTrailer(ctx, metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-unary"))
 
-	if r == nil {
-		return nil, errors.New("Request was empty")
+	if r == nil || r.RequestId == "" || r.Target == "" || len(r.Messages) == 0 {
+		return hub.defaultRequestLevelStatusCoding(500, r.RequestId, requestLevelProcessingErrorMessage), nil
+	} else if _, err := uuid.Parse(r.RequestId); err != nil {
+		return hub.defaultRequestLevelStatusCoding(500, r.RequestId, requestLevelProcessingErrorMessage), nil
 	}
 
-	if r.RequestId == "" {
-		return nil, errors.New("Missing Request ID")
-	}
-
-	if len(r.RequestId) != 36 {
-		return nil, errors.New("Request ID must be len 36 (uuid v4)")
-	}
-
-	if r.Target == "" {
-		return nil, errors.New("Missing Target")
-	}
-
-	if len(r.Messages) == 0 {
-		return nil, errors.New("Missing Messages")
-	}
+	//todo: If the DID targeted by a request object is not found within the Hub instance, the implementation **MUST** produce a request-level status with the code 404, and **SHOULD** use Target DID not found within the Identity Hub instance as the status text
 
 	for _, message := range r.Messages {
-		if message.Descriptor_ == nil {
-			return nil, errors.New("Missing descriptor")
+
+		if message.Descriptor_ == nil || message.Descriptor_.Method == "" ||
+			len(message.Descriptor_.DateCreated) < 10 ||
+			(message.Data != "" && message.Descriptor_.DataFormat == "") {
+			return hub.defaultMessageLevelStatusCoding(400, r.RequestId, improperlyConstructedErrorMessage), nil
+		} else if _, err := uuid.Parse(message.Descriptor_.ObjectId); err != nil {
+			return hub.defaultMessageLevelStatusCoding(400, r.RequestId, improperlyConstructedErrorMessage), nil
+		} else if _, ok := hub.validHubInterfaceMethods[message.Descriptor_.Method]; !ok {
+			return hub.defaultMessageLevelStatusCoding(501, r.RequestId, interfaceNotImplementedErrorMessage), nil
 		}
 
-		if message.Descriptor_.Method != "CollectionsWrite" {
-			return nil, errors.New("Descriptor method must be CollectionsWrite")
-		}
-
-		if len(message.Descriptor_.ObjectId) != 36 {
-			return nil, errors.New("Descriptor ObjectID must be length 36 (uuid v4)")
-		}
-
-		dateCreated := message.Descriptor_.DateCreated
-		if len(dateCreated) < 10 {
-			return nil, errors.New("Invalid date")
-		}
-
-		dateSeconds, _ := strconv.Atoi(dateCreated)
+		dateSeconds, _ := strconv.Atoi(message.Descriptor_.DateCreated)
 		t := time.Unix(int64(dateSeconds), 0)
 		Log.Info().Msg(t.String())
 	}
+
+	//todo: must have messageId which must be the stringified v1 CID of the associated message from its Request object
+	/*
+		pref := cid.Prefix{
+			Version: 1,
+			Codec: cid.Raw,
+			MhType: multihash.SHA2_256,
+			MhLength: -1, // default length
+		}
+		The object **MUST** have a messageId property,
+		and its value **MUST** be the stringified Version 1 CID of the associated message in the Request Object from which it was received.
+	*/
 
 	return &Response{
 		RequestId: r.RequestId,
 		Status: &Status{
 			Code: 200,
 		},
+		Replies: &Reply{
+			Status: &Status{
+				Code: 200,
+			},
+		},
 	}, nil
+}
+
+func (hub *IdentityHubService) defaultRequestLevelStatusCoding(code int64, requestId, message string) *Response {
+	return &Response{
+		RequestId: requestId,
+		Status: &Status{
+			Code:    code,
+			Message: message,
+		},
+	}
+}
+
+func (hub *IdentityHubService) defaultMessageLevelStatusCoding(code int64, requestId, message string) *Response {
+	return &Response{
+		RequestId: requestId,
+		Status: &Status{
+			Code: 200,
+		},
+		Replies: &Reply{
+			MessageId: "", //todo: its value **MUST** be the stringified Version 1 CID
+			Status: &Status{
+				Code:    code,
+				Message: message,
+			},
+		},
+	}
 }
--- api/identity_hub_test.go
+++ api/identity_hub_test.go
@@ -54,15 +54,20 @@ var _ = Describe("IdentityHub", func() {
 			if err != nil {
 				utils.Log.Error().Msg(err.Error())
 			}
+			Expect(err).To(BeNil())
 			Expect(response).To(Not(BeNil()))
 			Expect(response.RequestId).To(Equal(request.RequestId))
+			Expect(response.Status).To(Not(BeNil()))
 			Expect(response.Status.Code).To(Equal(int64(200)))
 		})
 
 		It("receives an error if Request is missing", func() {
 			request := &Request{}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Status).To(Not(BeNil()))
+			Expect(response.Status.Code).To(Equal(int64(500)))
 		})
 
 		It("receives an error if RequestID is missing", func() {
@@ -70,8 +75,11 @@ var _ = Describe("IdentityHub", func() {
 				Target:   "TheTarget",
 				Messages: validDemoMessages,
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Status).To(Not(BeNil()))
+			Expect(response.Status.Code).To(Equal(int64(500)))
 		})
 
 		It("receives an error if RequestID is not len 36 (uuid v4)", func() {
@@ -80,8 +88,11 @@ var _ = Describe("IdentityHub", func() {
 				Messages:  validDemoMessages,
 				RequestId: "shortrequestid",
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Status).To(Not(BeNil()))
+			Expect(response.Status.Code).To(Equal(int64(500)))
 		})
 
 		It("receives an error if Target is missing", func() {
@@ -89,8 +100,11 @@ var _ = Describe("IdentityHub", func() {
 				RequestId: "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
 				Messages:  validDemoMessages,
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Status).To(Not(BeNil()))
+			Expect(response.Status.Code).To(Equal(int64(500)))
 		})
 
 		It("receives an error if Messages are missing", func() {
@@ -98,8 +112,11 @@ var _ = Describe("IdentityHub", func() {
 				RequestId: "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
 				Target:    "atarget",
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Status).To(Not(BeNil()))
+			Expect(response.Status.Code).To(Equal(int64(500)))
 		})
 
 		It("receives an error if a Message is missing Descriptor", func() {
@@ -112,13 +129,17 @@ var _ = Describe("IdentityHub", func() {
 					},
 				},
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Replies).To(Not(BeNil()))
+			Expect(response.Replies.Status).To(Not(BeNil()))
+			Expect(response.Replies.Status.Code).To(Equal(int64(400)))
 		})
 
 		It("receives an error if a Message Descriptor is missing method", func() {
 			request := &Request{
-				RequestId: "09j23f09j23f0j",
+				RequestId: "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
 				Target:    "atarget",
 				Messages: []*Message{
 					{
@@ -127,8 +148,12 @@ var _ = Describe("IdentityHub", func() {
 					},
 				},
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Replies).To(Not(BeNil()))
+			Expect(response.Replies.Status).To(Not(BeNil()))
+			Expect(response.Replies.Status.Code).To(Equal(int64(400)))
 		})
 
 		It("receives an error if a Message Descriptor is missing objectID", func() {
@@ -146,8 +171,12 @@ var _ = Describe("IdentityHub", func() {
 					},
 				},
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Replies).To(Not(BeNil()))
+			Expect(response.Replies.Status).To(Not(BeNil()))
+			Expect(response.Replies.Status.Code).To(Equal(int64(400)))
 		})
 
 		It("receives an error if a Message Descriptor is missing dateCreated", func() {
@@ -164,8 +193,59 @@ var _ = Describe("IdentityHub", func() {
 					},
 				},
 			}
-			_, err := client.Process(ctx, request)
-			Expect(err).To(Not(BeNil()))
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Replies).To(Not(BeNil()))
+			Expect(response.Replies.Status).To(Not(BeNil()))
+			Expect(response.Replies.Status.Code).To(Equal(int64(400)))
+		})
+
+		It("receives an error if a Message has data but dataFormat is missing", func() {
+			request := &Request{
+				RequestId: "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
+				Target:    "atarget",
+				Messages: []*Message{
+					{
+						Data: "Data!",
+						Descriptor_: &MessageDescriptor{
+							Method:      "CollectionsWrite",
+							ObjectId:    "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
+							DateCreated: "1645917369",
+						},
+					},
+				},
+			}
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Replies).To(Not(BeNil()))
+			Expect(response.Replies.Status).To(Not(BeNil()))
+			Expect(response.Replies.Status.Code).To(Equal(int64(400)))
+		})
+
+		It("receives an error if a Message Descriptor method is not implemented", func() {
+			request := &Request{
+				RequestId: "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
+				Target:    "atarget",
+				Messages: []*Message{
+					{
+						Data: `{ "Data": "Test" }`,
+						Descriptor_: &MessageDescriptor{
+							Method:      "CollectionsWriteTest",
+							ObjectId:    "3eb8ea70-7ea5-4069-a153-cfb0ea682df9",
+							DateCreated: "1645917369",
+							DataFormat:  "application/json",
+						},
+					},
+				},
+			}
+			response, err := client.Process(ctx, request)
+			Expect(err).To(BeNil())
+			Expect(response).To(Not(BeNil()))
+			Expect(response.Replies).To(Not(BeNil()))
+			Expect(response.Replies.Status).To(Not(BeNil()))
+			Expect(response.Replies.Status.Code).To(Equal(int64(501)))
 		})
 	})
 })
@@ -179,7 +259,25 @@ func dialer() func(context.Context, string) (net.Conn, error) {
 
 	server := grpc.NewServer()
 
-	RegisterHubRequestServiceServer(server, &mockIdentityHubServer{})
+	RegisterHubRequestServiceServer(server, &mockIdentityHubServer{
+		IdentityHubService: IdentityHubService{
+			validHubInterfaceMethods: map[string]string{
+				"CollectionsQuery":   "",
+				"CollectionsWrite":   "",
+				"CollectionsCommit":  "",
+				"CollectionsDelete":  "",
+				"ThreadsQuery":       "",
+				"ThreadsCreate":      "",
+				"ThreadsReply":       "",
+				"ThreadsClose":       "",
+				"ThreadsDelete":      "",
+				"PermissionsRequest": "",
+				"PermissionsQuery":   "",
+				"PermissionsGrant":   "",
+				"PermissionsRevoke":  "",
+			},
+		},
+	})
 
 	go func() {
 		if err := server.Serve(listener); err != nil {
