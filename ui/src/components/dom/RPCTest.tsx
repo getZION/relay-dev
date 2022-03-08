81202b8f4898b7d66aec7693400775605d971b70
Christopher David <chris@arcade.city>
2022-03-08 11:37:57 -0600

    RPCtest cleanup

--- ui/src/components/dom/RPCTest.tsx
+++ ui/src/components/dom/RPCTest.tsx
@@ -1,26 +1,10 @@
 import { useEffect } from 'react'
 import { grpc } from '@improbable-eng/grpc-web'
 import useStore from '@/helpers/store'
-import { CommunitiesService } from 'proto/zion/v1/communities_pb_service'
 import { GetNodeInfoRequest } from 'proto/zion/v1/nodeinfo_pb'
-import { Community, CreateCommunityRequest } from 'proto/zion/v1/communities_pb'
 import { NodeInfoService } from 'proto/zion/v1/nodeinfo_pb_service'
-import { CollectionsWriteRequest } from 'proto/identityhub/v1/collections_pb'
-import { CollectionsService } from 'proto/identityhub/v1/collections_pb_service'
-import { Request } from 'proto/identityhub/v1/requests_pb'
 
 const nodeInfoRequest = new GetNodeInfoRequest()
-const collectionsWriteRequest = new CollectionsWriteRequest()
-const hubRequest = new Request()
-collectionsWriteRequest.setRequest(hubRequest)
-// hubRequest.setRequestid
-// collectionsWriteRequest.
-
-// const createCommunityRequest = new CreateCommunityRequest()
-// const community = new Community()
-// community.setName('Hello World Community! !! ! !!')
-// community.setDescription('AMAZING DESCRIPTION ! ! !! ')
-// createCommunityRequest.setPayload(community)
 
 interface NodeInfoObject {
   pubkey: string
@@ -50,22 +34,6 @@ export const RPCTest = () => {
         }
       },
     })
-
-    // grpc.unary(CollectionsService.CollectionsWrite, {
-    //   request: collectionsWriteRequest,
-    //   host: 'http://localhost:9090',
-    //   onEnd: (res) => {
-    //     console.log('CollectionsWrite response:', res)
-    //   },
-    // })
-
-    // grpc.unary(CommunitiesService.CreateCommunity, {
-    //   request: createCommunityRequest,
-    //   host: 'http://localhost:9090',
-    //   onEnd: (res) => {
-    //     console.log('CreateCommunity response:', res)
-    //   },
-    // })
   }, [])
   return (
     <>
