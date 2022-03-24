e61156e187fdbc99a60ec5c40c36dfa71d78db7b
chrishein3 <chrishein931@gmail.com>
2022-03-24 15:16:54 -0400

    Fetch response model updated

--- api/api.go
+++ api/api.go
@@ -3,17 +3,20 @@ package api
 type (
 	Community struct {
 		Id              int64
-		Name            string   `json:"Name" validate:"required,max=150"`
-		Description     string   `json:"Description" validate:"max=250"`
-		OwnerDid        string   `json:"OwnerDid" validate:"required"`
-		OwnerUsername   string   `json:"OwnerUsername" validate:"required"`
-		PricePerMessage int64    `json:"PricePerMessage" validate:"gte=0,lt=100000"`
-		PriceToJoin     int64    `json:"PriceToJoin" validate:"gte=0,lt=100000"`
-		EscrowAmount    int64    `json:"EscrowAmount" validate:"gte=0,lt=100000"`
-		Img             string   `json:"Img"`
-		Created         int64    `json:"Created"`
-		LastActive      int64    `json:"LastActive"`
-		Public          bool     `json:"Public"`
+		Zid             string
+		Name            string `json:"Name" validate:"required,max=150"`
+		Description     string `json:"Description" validate:"max=250"`
+		OwnerDid        string `json:"OwnerDid" validate:"required"`
+		OwnerUsername   string `json:"OwnerUsername" validate:"required"`
+		PricePerMessage int64  `json:"PricePerMessage" validate:"gte=0,lt=100000"`
+		PriceToJoin     int64  `json:"PriceToJoin" validate:"gte=0,lt=100000"`
+		EscrowAmount    int64  `json:"EscrowAmount" validate:"gte=0,lt=100000"`
+		Img             string `json:"Img"`
+		Created         int64  `json:"Created"`
+		LastActive      int64  `json:"LastActive"`
+		Public          bool   `json:"Public"`
+		Deleted         bool
+		Updated         int64
 		Tags            []string `json:"Tags" validate:"max=5"`
 		Users           []string
 	}
