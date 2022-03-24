c2c930688cb3e6eced5db6c6094b183f685d94e7
chrishein3 <chrishein931@gmail.com>
2022-03-24 13:39:55 -0400

    community fetch response model updated

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@ package api
 
 type (
 	Community struct {
+		Id              int64
 		Name            string   `json:"Name" validate:"required,max=150"`
 		Description     string   `json:"Description" validate:"max=250"`
 		OwnerDid        string   `json:"OwnerDid" validate:"required"`
@@ -10,6 +11,7 @@ type (
 		PriceToJoin     int64    `json:"PriceToJoin" validate:"gte=0,lt=100000"`
 		EscrowAmount    int64    `json:"EscrowAmount" validate:"gte=0,lt=100000"`
 		Tags            []string `json:"Tags" validate:"max=5"`
+		Users           []string
 	}
 
 	JoinCommunity struct {
